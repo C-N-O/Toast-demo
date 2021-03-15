e4c484626046eb343be2ce828f4ccdf26926329f
C-N-O <meetmancliff@yahoo.com>
2021-03-14 23:46:20 -0500

    icons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
-function App() {
+toast.configure();
+
+const CustomToast = ({ closeToast }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Oops! something went wrong <button onClick={closeToast}>close</button>
+    </div>
+  );
+};
+
+function App() {
+  const notify = () => {
+    toast('You clicked the button', { position: toast.POSITION.TOP_LEFT });
+
+    toast.success('Success', {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 8000,
+    });
+
+    toast.info('Info', { position: toast.POSITION.TOP_RIGHT });
+
+    toast.warn('Warning', {
+      position: toast.POSITION.BOTTOM_LEFT,
+      autoClose: false,
+    });
+
+    toast.error('Error', {
+      position: toast.POSITION.BOTTOM_CENTER,
+      autoClose: 10000,
+    });
+
+    toast.warn(<CustomToast />, {
+      position: toast.POSITION.BOTTOM_RIGHT,
+      autoClose: false,
+    });
+  };
+
+  return (
+    <div className='App'>
+      <button onClick={notify}>Notify</button>
     </div>
   );
 }
 
 export default App;
+
+//By default, the notifications close after 5 secons
+//autoClose: false = toast will not auto close
+//You can also create a custom toast
